65a26f7f586719b02679769faba733fb7e03fa1e
Fabricio Vianna <fabriciohenriquemviana@gmail.com>
2024-02-04 10:45:36 -0300

    feat: teams migration

---
+++ database/migrations/2024_02_04_130553_teams.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->id('team_id');
+            $table->string('name', 255)->nullable(false);
+            $table->string('description', 255)->nullable(true);
+            $table->binary('icon')->nullable(true);
+            $table->date('birthday')->nullable(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
