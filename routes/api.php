96e0d26b55aa0274b6174c54d9e37f3aab13e241
Fabricio Vianna <fabriciohenriquemviana@gmail.com>
2024-02-04 10:01:24 -0300

    fix: removing refresh token auth

--- routes/api.php
+++ routes/api.php
@@ -15,18 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
-], function ($router) {
-
+], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('logout', [AuthController::class, 'logout']);
-    Route::post('refresh', [[AuthController::class, 'refresh']]);
     Route::post('me', [AuthController::class, 'me']);
+});
+
+
+Route::middleware('api')->get('/user', function (Request $request) {
 });
