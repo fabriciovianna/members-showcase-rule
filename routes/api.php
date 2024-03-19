ca0d93938488d487824da05a34344e4bcefeb337
Fabricio Vianna <fabriciohenriquemviana@gmail.com>
2024-03-18 22:50:14 -0300

    feat: jwt auth validator

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+    'prefix' => 'auth'
+], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('me', [AuthController::class, 'me']);
+});
 
 Route::group([
     'prefix' => 'artisan/seed/'
